66c90f04eddd0f745a501d4dbd9517d63166a348
MFukazawa <micahfukazawa@gmail.com>
2020-10-05 14:39:09 +0900

    [Update] Admin page + add styling

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="about-page">
-    <h1>This is my absolutely fantastic "About Me" Page!</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+    <h1>Nuxt Blog with Firebase</h1>
+    <ul class="contents">
+      <li>Nuxt.js</li>
+      <li>Firebase Database</li>
+      <li>Firebase Email/Password Login</li>
+    </ul>
   </div>
 </template>
 
@@ -12,5 +13,42 @@
 .about-page {
   text-align: center;
   padding: 30px;
+}
+
+.contents {
+  margin: 0 auto;
+  padding: 2rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.contents > li {
+  font-size: 1.5rem;
+  flex: 0 1 auto;
+  margin: 1rem;
+  padding: 2rem;
+  list-style: none;
+  background-color: darkslategray;
+  color: white;
+  border-radius: 50px;
+  border: silver 2px solid;
+}
+
+.contents > li:hover {
+  transform: translateY(-10px);
+  transition-duration: .2s;
+  transition-timing-function: ease-in-out;
+}
+
+@media screen and (min-width: 768px) {
+  .contents {
+    flex-direction: row;
+  }
+
+  .contents > li {
+    margin: 0 2rem;
+  }
 }
 </style>
